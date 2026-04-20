a4e94ab563cedce479a94da88c752d92d67d579e
khalidhasan4a-ux <khalidhasan4a@gmail.com>
2026-04-20 07:54:19 +0600

    Add Java example demonstrating OOP concepts

---
+++ Day 01/Example 02.java
@@ -0,0 +1,47 @@
+// Code 2: Inheritance, Encapsulation, Polymorphism
+
+class Animal {
+
+    // Encapsulation (private variable)
+    private String name;
+
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Method
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+// Inheritance
+class Dog extends Animal {
+
+    // Polymorphism (method overriding)
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog d1 = new Dog();
+
+        // Using encapsulation
+        d1.setName("Tommy");
+
+        System.out.println("Dog Name: " + d1.getName());
+
+        // Polymorphism
+        d1.sound();
+    }
+}
