8e628bbdf85a07f059ed19555da3a94a1705ff97
khalidhasan4a-ux <khalidhasan4a@gmail.com>
2026-04-20 07:53:59 +0600

    Add Student class and main method example

---
+++ Day 01/Example 01.java
@@ -0,0 +1,28 @@
+// Code 1: Class, Object, Properties, Methods
+
+class Student {
+    // Properties (Attributes)
+    String name;
+    int age;
+
+    // Method
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Creating object
+        Student s1 = new Student();
+
+        // Assigning values
+        s1.name = "Jadid";
+        s1.age = 20;
+
+        // Calling method
+        s1.displayInfo();
+    }
+}
