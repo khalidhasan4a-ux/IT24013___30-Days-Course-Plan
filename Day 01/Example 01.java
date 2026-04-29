ae63f93cb5c10cfb75c6830dd31def4d7d1d332a
khalidhasan4a-ux <khalidhasan4a@gmail.com>
2026-04-29 12:48:56 +0600

    Refactor Student class to Car class with display method

--- Day 01/Example 01.java
+++ Day 01/Example 01.java
@@ -1,26 +1,40 @@
-class Student {
-    // Properties 
-    String name;
-    int age;
+class Car {
+
+    // Properties
+    String brand;
+    String color;
+    int speed;
+
+    // Constructor
+    Car(String b, String c, int s) {
+        brand = b;
+        color = c;
+        speed = s;
+    }
 
     // Method
     void displayInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
+        System.out.println("Car Brand : " + brand);
+        System.out.println("Car Color : " + color);
+        System.out.println("Car Speed : " + speed + " km/h");
     }
 }
 
-public class Main {
+// Main Class
+public class IT24013 {
+
     public static void main(String[] args) {
 
-        // Creating object
-        Student s1 = new Student();
+        // Creating Objects
+        Car car1 = new Car("Toyota", "Red", 120);
+        Car car2 = new Car("BMW", "Black", 180);
 
-        // Assigning values
-        s1.name = "Jadid";
-        s1.age = 20;
+        System.out.println("Car 1 Information:");
+        car1.displayInfo();
 
-        // Calling method
-        s1.displayInfo();
+        System.out.println();
+
+        System.out.println("Car 2 Information:");
+        car2.displayInfo();
     }
 }
