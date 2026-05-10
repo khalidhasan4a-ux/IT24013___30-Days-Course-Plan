c8fefe4fd6155f410d82c834fcdede8597cc0536
khalidhasan4a-ux <khalidhasan4a@gmail.com>
2026-05-10 19:39:14 +0600

    Create Example 02.java

---
+++ Day 20/Example 02.java
@@ -0,0 +1,15 @@
+import java.time.LocalDateTime;
+
+public class IT24013 {
+
+    public static void main(String[] args) {
+
+        // Get current date and time
+        LocalDateTime now =
+                LocalDateTime.now();
+
+        System.out.println(
+                "Current date and time: "
+                + now);
+    }
+}
